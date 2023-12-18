e0d31db124eaa1cc2c06d53e32ec894e3a7f06b6
Francesco Cannizzo <fcannizzo97@gmail.com>
2023-12-18 17:33:24 +0100

    aggiunga seconda immagine (ho visto solo ora che erano 2)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Button label='Bottone TOP' />
-        <ImageClass url='https://s3.ap-south-1.amazonaws.com/curiousjr-public/web/banner_what_is_coding_1639724390969.jpg' />
+        <ImageClass url='https://s3.ap-south-1.amazonaws.com/curiousjr-public/web/banner_what_is_coding_1639724390969.jpg' margin='20px 0 0 0'/>
+        <ImageClass url='https://www.noidellascuola.it/wp-content/uploads/2022/12/shutterstock_1714491562.jpg' margin='0 0 20px 0'/>
         <Button label='Bottone BOT' />
       </header>
     </div>
